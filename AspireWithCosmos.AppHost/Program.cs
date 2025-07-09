15947a60bb8cec40a89aeb2d0931d6af1cd5af50
Guanzhou Song <guanzhousong@microsoft.com>
2025-07-09 20:10:21 +0000

    check

--- AspireWithCosmos.AppHost/Program.cs
+++ AspireWithCosmos.AppHost/Program.cs
@@ -2,11 +2,23 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+#pragma warning disable ASPIRECOSMOSDB001
 var cosmos = builder.AddAzureCosmosDB("cosmos")
-    // Remove the RunAsEmulator() line should you want to use a live instance during development
-    .RunAsEmulator();
+.RunAsPreviewEmulator(emulator =>
+    {
+        emulator
+            .WithContainerName("myservice-cosmosdb")
+            .WithDataExplorer(1234)
+            .WithOtlpExporter()
+            .WithDataVolume()
+            .WithLifetime(ContainerLifetime.Persistent);
+    });
+#pragma warning restore ASPIRECOSMOSDB001
 
-var iotdb = cosmos.AddCosmosDatabase("iotdb"); 
+var iotdb = cosmos.AddCosmosDatabase("iotdb");
+var userDb = cosmos.AddCosmosDatabase("userdb");
+var usersContainer = userDb.AddContainer("container", "/id");
+
 
 var apiService = builder.AddProject<Projects.AspireWithCosmos_ApiService>("apiservice")
     .WithReference(iotdb);
